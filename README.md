4e65828a3aed26eb83f825e78b6ae1eaf76857fd
Nope Nope <05.tycoon.malaise@icloud.com>
2023-07-24 16:06:00 -0500

    Initial Commit
    uses log-likelihood and p-value as a heuristic for confidence which appends and routes the message outputs appropriately

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# Squidward Tentacles and Spying on Outputs via Conformal Prediction
- A computationally expensive, but vitally important means of interrogating non-conformity of a given output to an established distribution of outputs by a language model for the purpose of using p-values as a de-factor confidence interval whereby we sort to various SQS queues and append messaging that allows for more precise utilization of output
+## ♫ The Dream of the 90's ♫ is alive in ~~Portland~~ ["a weird suite of Enterprise LLM tools"](https://github.com/users/rabbidave/projects/1) named after [Nicktoons](https://en.wikipedia.org/wiki/Nicktoons)
+### by [some dude in his 30s](https://www.linkedin.com/in/davidisaacpierce)
+#
+## Utility 4) # Squidward Tentacles and Spying on Outputs via Conformal Prediction
+
+<img src="https://upload.wikimedia.org/wikipedia/en/thumb/8/8f/Squidward_Tentacles.svg/1200px-Squidward_Tentacles.svg.png" alt="Squidward" title="Squidward" width="40%">
+
+## Description:
+A set of serverless functions designed to assist in the monitoring of outputs from language models, specifically inspection of messages for non-conformity to pre-calulcated log-likelihood and the appending of warnings based on the achieved log-likelihood and p-values for new messages
+
+Note: BERT was established as default, given the flexibility in swapping transformers models, but the quality of your model and resultant log-likelihood has a direct effect on the quality of your p-values and their utility as an indicator of non-conformity
+
+#
+## Rationale:
+
+1) Large Language Models are [subject to various forms of prompt injection](https://github.com/greshake/llm-security) ([indirect](https://github.com/greshake/llm-security#compromising-llms-using-indirect-prompt-injection) or otherwise); lightweight and step-wise alerting of outputs ensures additional safeguarding of externally facing models
+2) User experience, instrumentation, and metadata capture are crucial to the adoption of LLMs for orchestration of [multi-modal agentic systems](https://en.wikipedia.org/wiki/Multi-agent_system); calculating the log-likehood of a given message, and thus it's p-value, serves as a good heuristics for a confidence interval of the given output.
+
+#
+## Intent:
+
+The intent of this Squidward_looking_out_his_window.py is to efficiently spin up, calculate needed values for evaluation, and inspect each message for non-conformity to established output; thereafter routing messages to the appropriate SQS bus (e.g. for response to user, further evaluation, logging, etc)
+
+The goal being to detect if the message has high non-conformity with known model outputs; via use of a pre-calculated log-likelihood for the model (or eventually specific vector space clusters)
+    
+The is pre-calculated and stored in parquet, then loaded into memory. log-likelihood and p-values are calculated for incoming messages and appended/routed appropriately; when complete the function spins down appropriately. 
+
+Based on the resultant calculations messages are routed to the appropriate SQS bus.
+
+#
+### Note: Needs additional error-handling; this is mostly conceptual and assumes the use of environment variables rather than hard-coded values for confidence/p-values. Also contained arbitrary decisions about p-value ties.
---
+++ Squidward_looking_out_his_window.py
@@ -0,0 +1,137 @@
+# My thanks to the open-source community; LLMs brought about a GNU revival
+# My thanks to Arize AI in particular; y'all inspired this and other utilities with an awesome observability platform & sessions
+# Note: This Lambda has a timeout to ensure it's spun down gracefully; manage your Lambda with Provisioned Concurrency to ensure SQS messages don't get missed
+# Note: This Lambda has a non-trivial computational cost and is intended to monitor and append messages based on the non-conformity to a representative distribution of outputs
+# i.e. it's used to validate stuff like externally facing LLMs using an objective measure of "confidence" given a specific output
+
+
+import pandas as pd
+import boto3
+import time
+import json
+import os
+from botocore.exceptions import ClientError
+import datetime
+import logging
+import random
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
+
+# Define constants
+CONFIDENCE_THRESHOLD_HIGH = 0.99
+CONFIDENCE_THRESHOLD_LOW = 0.95
+SQS_QUEUE_URL = 'https://sqs.us-east-1.amazonaws.com/123456789012/MyQueue'
+SAFE_SQS_QUEUE_URL = 'https://sqs.us-east-1.amazonaws.com/123456789012/MyFirstQueue'
+STANDARD_SQS_QUEUE_URL = 'https://sqs.us-east-1.amazonaws.com/123456789012/MySecondQueue'
+LOW_CONFIDENCE_SQS_QUEUE_URL = 'https://sqs.us-east-1.amazonaws.com/123456789012/MyThirdQueue'
+S3_BUCKET_NAME = 'my-bucket'
+BASELINE_LOG_LIKELIHOODS_FILE_KEY = 'baseline_log_likelihoods.parquet'
+RETRY_COUNT = 3  # Define a suitable retry count
+LANGUAGE_MODEL = 'gpt2'  # Specify the language model to use
+
+# Initialize boto3 clients
+s3 = boto3.client('s3')
+sqs = boto3.client('sqs')
+
+# Initialize language model and tokenizer
+tokenizer = GPT2Tokenizer.from_pretrained(LANGUAGE_MODEL)
+model = GPT2LMHeadModel.from_pretrained(LANGUAGE_MODEL)
+
+# Setup logging
+logging.basicConfig(level=logging.INFO)
+
+# Load the baseline log-likelihoods from S3
+try:
+    obj = s3.get_object(Bucket=S3_BUCKET_NAME, Key=BASELINE_LOG_LIKELIHOODS_FILE_KEY)
+    baseline_log_likelihoods = pd.read_parquet(obj['Body'])
+except ClientError as e:
+    logging.error(f'Error loading baseline log-likelihoods: {e}', exc_info=True)
+    raise
+
+def compute_log_likelihood(text):
+    inputs = tokenizer(text, return_tensors='pt')
+    outputs = model(inputs, labels=inputs)
+    return -outputs.loss.item()
+
+def compute_p_value(log_likelihood):
+    return ((baseline_log_likelihoods >= log_likelihood).sum() + 0.5) / (len(baseline_log_likelihoods) + 1)
+
+def send_message_to_sqs(df, queue_url):
+    try:
+        message = {
+            'MessageBody': df.to_json(),
+            'QueueUrl': queue_url
+        }
+        sqs.send_message(**message)
+    except Exception as e:
+        logging.error(f"Error sending message: {e}", exc_info=True)
+        raise
+
+def process_data(df):
+    log_likelihood = compute_log_likelihood(df['text'])
+    p_value = compute_p_value(log_likelihood)
+    if p_value >= CONFIDENCE_THRESHOLD_HIGH:
+        send_message_to_sqs(df["message"], SAFE_SQS_QUEUE_URL)
+    elif CONFIDENCE_THRESHOLD_LOW <= p_value < CONFIDENCE_THRESHOLD_HIGH:
+        df["message"] = df["message"] + " Note: this message achieved a confidence interval of 95% via conformal prediction; meaning there is still a 1/20 chance of error."
+        send_message_to_sqs(df["message"], STANDARD_SQS_QUEUE_URL)
+    else:
+        df["message"] = df["message"] + " Warning: this message achieved a confidence interval of below 95% using conformal prediction; meaning there is a greater than 1/20 chance of error. Use this output with caution."
+        send_message_to_sqs(df["message"], LOW_CONFIDENCE_SQS_QUEUE_URL)
+
+
+def receive_message():
+    try:
+        response = sqs.receive_message(
+            QueueUrl=SQS_QUEUE_URL,
+            AttributeNames=['All'],
+            MaxNumberOfMessages=10,
+            MessageAttributeNames=['All'],
+            VisibilityTimeout=60,
+            WaitTimeSeconds=20
+        )
+    except ClientError as e:
+        logging.error(f'Error receiving message: {e}', exc_info=True)
+        time.sleep((2 ** RETRY_COUNT) + random.uniform(0, 1))  # Exponential backoff
+        RETRY_COUNT -= 1
+        if RETRY_COUNT == 0:
+            raise
+    return response
+
+def lambda_handler(event, context):
+    start_time = time.time()
+
+    while True:
+        # Check if 10 minutes have passed
+        if time.time() - start_time > 600:
+            break
+
+        try:
+            response = receive_message()
+
+            if 'Messages' in response:
+                for message in response['Messages']:
+                    receipt_handle = message['ReceiptHandle']
+
+                    try:
+                        body = json.loads(message['Body'])
+                        df = pd.DataFrame(body)
+
+                        process_data(df)
+
+                        # Delete message after successful processing
+                        sqs.delete_message(
+                            QueueUrl=SQS_QUEUE_URL,
+                            ReceiptHandle=receipt_handle
+                        )
+                    except Exception as e:
+                        logging.error(f'Error processing message: {e}', exc_info=True)
+                        raise
+            else:
+                # No more messages in the queue, terminate the function
+                break
+        except ClientError as e:
+            logging.error(f'Error receiving message: {e}', exc_info=True)
+            time.sleep((2 ** RETRY_COUNT) + random.uniform(0, 1))  # Exponential backoff
+            RETRY_COUNT -= 1
+            if RETRY_COUNT == 0:
+                raise
